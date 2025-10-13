f7590c08377e4eb936b28f597bee1c78564d3696
Maquita <maria.parram@udea.edu.co>
2025-10-12 21:05:03 -0500

    Se crea la base del proyecto en Java, además se crean las clases: Usuario y Paciente.

---
+++ src/app/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
