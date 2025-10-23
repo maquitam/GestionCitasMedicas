97e6e777435544b474b95379a50cc5a4a15c96a1
AlejandroRuizG <alejandro.ruizg@udea.edu.co>
2025-10-22 21:53:38 -0500

    Creación de clase Especialidad

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,9 +3,13 @@ package app;
 
 import javax.swing.*;
 import UI.LoginView;
+import objetos.Especialidad;
 
 public class Main {
     public static void main(String[] args) {
-        LoginView login = new LoginView();
+        //LoginView login = new LoginView();
+        Especialidad esp = new Especialidad("Cardiologia", "ESP001", true);
+        System.out.println(esp.consultarEspecialidad());
+    
     }
 }
--- src/objetos/Paciente.java
+++ src/objetos/Paciente.java
@@ -1,5 +1,5 @@
 package objetos;
 
 public class Paciente {
-    
+
 }
