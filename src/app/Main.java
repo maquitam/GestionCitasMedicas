80244b18674b3b9c3256a0edf3a7d76296024d42
Sebas <sebastian.cardona7@udea.edu.co>
2025-11-09 23:36:42 -0500

    prueba

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,7 +4,7 @@ import UI.LoginView;
 import UI.PacienteView;
 import objetos.Paciente;
 //import servicios.UsuarioSesion;
-
+//hola
 public class Main {
 
     public static void main(String[] args) {
